6f07a3dbb3e20ae8b69a0df26d34a6e0f6e6db06
vedantsharmascaler <vedant.sharma_1@scaler.com>
2025-02-28 11:22:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with me!! Yay
+          Learn React with Vedant!! Yay
         </a>
       </header>
     </div>
